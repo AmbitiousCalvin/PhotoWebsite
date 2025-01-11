995be2ad078c0193379f63be655958e06a59887b
CalvinTaw <calvin6taw@gmail.com>
2025-01-12 00:54:05 +0630

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import "./styles/styles.css";
 import "./styles/loading.css";
 import "./styles/error.css";
 
-import { Homepage } from "./pages/Homepage.js";
-import { Preview } from "./pages/Preview";
-import { Videopage } from "./pages/Videopage";
-import { NotFoundpage } from "./pages/NotFound";
 import { Header } from "./components/header";
 import { InitialLoading } from "./components/loading";
 import { ScrollToTopButton } from "./components/ScrollToTopBtn";
 import ScrollToTop from "./components/ScrollToTop";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createContext, useState, useMemo, useEffect } from "react";
+import { lazy, Suspense } from "react";
+
 export const MyContext = createContext(null);
+
+// Lazy load the pages
+const Homepage = lazy(() =>
+  import("./pages/Homepage").then((module) => ({ default: module.Homepage }))
+);
+const Preview = lazy(() =>
+  import("./pages/Preview").then((module) => ({ default: module.Preview }))
+);
+const Videopage = lazy(() =>
+  import("./pages/Videopage").then((module) => ({ default: module.Videopage }))
+);
+const NotFoundpage = lazy(() =>
+  import("./pages/NotFound").then((module) => ({
+    default: module.NotFoundpage,
+  }))
+);
 
 const popularPhotoQueries = [
   "nature",
@@ -75,21 +89,21 @@ export default function App() {
   );
 
   return (
-    <>
-      <MyContext.Provider value={contextValue}>
-        {<ScrollToTopButton />}
-        {isLoading && <InitialLoading />}
-        <ScrollToTop />
-        <Header />
+    <MyContext.Provider value={contextValue}>
+      {<ScrollToTopButton />}
+      {isLoading && <InitialLoading />}
+      <ScrollToTop />
+      <Header />
 
+      <Suspense fallback={<InitialLoading />}>
         <Routes>
-          {/* <Route path="/" element={<Homepage />} /> */}
+          <Route path="/" element={<Homepage />} />
           <Route path="/photos" element={<Homepage />} />
           <Route path="/video" element={<Videopage />} />
           <Route path="/preview" element={<Preview />} />
-          <Route path="/" element={<NotFoundpage />} />
+          <Route path="*" element={<NotFoundpage />} />
         </Routes>
-      </MyContext.Provider>
-    </>
+      </Suspense>
+    </MyContext.Provider>
   );
 }
--- src/pages/Preview.js
+++ src/pages/Preview.js
@@ -1,18 +1,59 @@
 import { MainContent } from "../main";
-import { MyContext } from "../App";
-import "../styles/slider.css";
-import { useState, useEffect, useCallback, useContext, memo } from "react";
+import {
+  InputContainer,
+  DropDown,
+  DropDownListItem,
+  Icon,
+} from "../components/general";
+import "../styles/preview-page.css";
 
 function Preview() {
-  const { query, order } = useContext(MyContext);
-
   return (
-    <>
-      <div>hello this is preview</div>
-    </>
+    <div className="preview-page">
+      <div className="preview-container">
+        <div className="preview-header">
+          <div className="user-container">
+            <img
+              src="user-avatar.jpg"
+              alt="User Avatar"
+              className="user-avatar"
+            />
+            <div className="flex-column">
+              <p className="user-name">Username</p>
+              <div className="links">
+                <a href="#follow" className="link">
+                  Follow
+                </a>
+                <span className="circle">•</span>
+                <a href="#donate" className="link">
+                  Donate
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="icon-container">
+            <div className="btn btn-border btn-square">
+              <Icon class="fas fa-thumbs-up"></Icon>
+            </div>
+            <div className="btn btn-border btn-square">
+              <Icon class="fa-regular fa-heart"></Icon>
+              Like 42
+            </div>
+            <div className="btn btn-border btn-square">
+              <Icon class="fa-regular fa-bookmark"></Icon>
+              Collect
+            </div>
+            <div className="btn btn-border btn-square">
+              <Icon class="fa-solid fa-solid-nodes"></Icon>
+            </div>
+          </div>
+        </div>
+        <div className="img-slider">Image Slider Content Here</div>
+        <div className="more-info">More Info Content Here</div>
+      </div>
+    </div>
   );
 }
 
-const MemoizedPreview = memo(Preview);
-
-export { MemoizedPreview as Preview };
+export { Preview };
